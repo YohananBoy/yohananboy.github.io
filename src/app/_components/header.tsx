da6443cbf58ad6f0caa821c12f19f4064c283716
Yohanan <14911025786@cefet-rj.br>
2026-04-11 14:50:31 -0300

    feat: initial commit

---
+++ src/app/_components/header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header>
+      <h1 className="text-7xl">Yohanan Boy</h1>
+      <h2 className="text-4xl">3° ano Informática</h2>
+    </header>
+  );
+}
---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import "~/styles/globals.css";
+
+import { type Metadata } from "next";
+import { Geist } from "next/font/google";
+import Header from "./_components/header";
+
+export const metadata: Metadata = {
+  title: "Yohanan Boy - Portifólio",
+  description:
+    "Portifólio de Yohanan Boy, aluno do CEFET-RJ/NF. Aprendiz de HTML, CSS, C++, PHP, JavaScript, TypeScript, Tailwind, MySQL e etc",
+  icons: [{ rel: "icon", url: "/check2.svg" }],
+};
+
+const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist-sans",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" className={`${geist.variable}`}>
+      <body>
+        <Header></Header>
+        {children}
+      </body>
+    </html>
+  );
+}
